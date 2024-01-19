c5cf3ecbb6862416ef70c2f002859c96acb2d62e
jvras58 <jvras@cin.ufpe.br>
2024-01-19 15:30:31 -0300

    configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from app.app import app
+
+
+def test_root_deve_retornar_200_e_hello_world():
+
+    client = TestClient(app)
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Hello World'}
